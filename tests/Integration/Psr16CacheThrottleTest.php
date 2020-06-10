fd26b732b0a883ab0637de0c82089d2967abdfd0
Kevin Bond <kevinbond@gmail.com>
2020-06-10 09:21:45 -0400

    enable Psr16CacheThrottleTest to work with --prefer-lowest

--- tests/Integration/Psr16CacheThrottleTest.php
+++ tests/Integration/Psr16CacheThrottleTest.php
@@ -5,6 +5,7 @@ namespace Zenstruck\Governator\Tests\Integration;
 use Symfony\Component\Cache\Adapter\FilesystemAdapter;
 use Symfony\Component\Cache\CacheItem;
 use Symfony\Component\Cache\Psr16Cache;
+use Symfony\Component\Cache\Simple\FilesystemCache;
 use Zenstruck\Governator\Store;
 use Zenstruck\Governator\Store\Psr16CacheStore;
 
@@ -13,22 +14,20 @@ use Zenstruck\Governator\Store\Psr16CacheStore;
  */
 final class Psr16CacheThrottleTest extends ThrottleTest
 {
-    public static function setUpBeforeClass(): void
-    {
-        if (!\class_exists(Psr16Cache::class)) {
-            self::markTestSkipped('Psr16Cache not available.');
-        }
-    }
-
     protected static function clockMockClasses(): iterable
     {
         yield FilesystemAdapter::class;
+        yield FilesystemCache::class;
         yield 'Symfony\Component\Cache\Traits\FilesystemTrait';
         yield CacheItem::class;
     }
 
     protected function createStore(): Store
     {
-        return new Psr16CacheStore(new Psr16Cache(new FilesystemAdapter()));
+        if (\class_exists(Psr16Cache::class)) {
+            return new Psr16CacheStore(new Psr16Cache(new FilesystemAdapter()));
+        }
+
+        return new Psr16CacheStore(new FilesystemCache());
     }
 }
