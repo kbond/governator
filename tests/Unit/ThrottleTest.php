5d162bfcfb4412ad07b966f76ae5edb33d934305
Kevin Bond <kevinbond@gmail.com>
2020-06-11 14:53:33 -0400

    [minor] ensure negative block for is treated like zero

---
+++ tests/Unit/ThrottleTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Zenstruck\Governator\Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+use Zenstruck\Governator\Exception\QuotaExceeded;
+use Zenstruck\Governator\Key;
+use Zenstruck\Governator\Store\MemoryStore;
+use Zenstruck\Governator\Throttle;
+
+/**
+ * @author Kevin Bond <kevinbond@gmail.com>
+ */
+final class ThrottleTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function negative_blocks_for_has_same_effect_as_zero(): void
+    {
+        $throttle = new Throttle(new MemoryStore(), new Key('foo', 1, 10));
+
+        $throttle->hit();
+
+        $start = time();
+
+        try {
+            $throttle->hit(-20);
+        } catch (QuotaExceeded $e) {
+            $this->assertSame(time(), $start);
+
+            return;
+        }
+
+        $this->fail('Exception not thrown.');
+    }
+}
